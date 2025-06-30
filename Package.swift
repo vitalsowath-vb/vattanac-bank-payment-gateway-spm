5f0111bb132689ad2f215bd50fdf752620ed508f
vitalsowath <vitalsowath@gmail.com>
2025-06-30 16:32:44 +0700

    Update checksum for version 1.5.8

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "VattanacPaySDK",
             url: "https://github.com/vitalsowath-vb/vattanac-bank-payment-gateway-spm/raw/main/VattanacPaySDK.xcframework.zip",
-            checksum: "973cf22d1fb306de2071d5e7c658ec819c0fe69b84ac7465c4eefb60363a233c"
+            checksum: "b6a6b59e828e4edf3c25017f49c6d33edc21c8a37657c33e63225223c4b18b00"
         )
     ]
 )
